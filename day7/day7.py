24a406942be097243ccc5b8db55ce99a70740eeb
Chuck Woodraska <chuck.woodraska@gmail.com>
2018-12-11 16:38:28 -0500

    Day 9 WIP. CW

---
+++ day7/day7.py
@@ -0,0 +1,30 @@
+import re
+from networkx import DiGraph, lexicographical_topological_sort
+
+with open('input.txt', 'r') as f:
+    data = f.read().splitlines()
+
+    graph = DiGraph()
+    # PART 1
+    for line in data:
+        line_search = re.search(r'Step ([A-Z]) must be finished before step ([A-Z])', line)
+        node = line_search.group(1)
+        link_node = line_search.group(2)
+        graph.add_edge(node, link_node)
+    print(''.join(lexicographical_topological_sort(graph)))
+
+    # PART 2
+    for node in graph.nodes:
+        graph.nodes[node]['work'] = 61 + ord(node) - ord('A')
+
+    total_seconds = 0
+
+    while graph.nodes:
+        available_nodes = [node for node in graph.nodes if not graph.in_degree(node)]
+
+        for worker, node in zip(range(5), available_nodes):
+            graph.nodes[node]['work'] -= 1
+            if not graph.nodes[node]['work']:
+                graph.remove_node(node)
+        total_seconds += 1
+    print(total_seconds)
---
+++ day8/day8.py
@@ -0,0 +1,36 @@
+def count_meta(node, total):
+    child_nodes = node.pop(0)
+    meta_nodes = node.pop(0)
+    for child in range(child_nodes):
+        total = count_meta(node, total)
+    for meta in range(meta_nodes):
+        total += node.pop(0)
+    return total
+
+def count_meta2(node, total, lookup, _id):
+    child_nodes = node.pop(0)
+    meta_nodes = node.pop(0)
+    for child in range(1, child_nodes+1):
+        lookup[child] = count_meta2(node, total, {}, child)
+    mid_total = 0
+    if child_nodes:
+        for meta in range(meta_nodes):
+            meta_index = node.pop(0)
+            mid_total += lookup.get(meta_index, 0)
+    else:
+        for meta in range(meta_nodes):
+            meta_index = node.pop(0)
+            mid_total += meta_index
+    return mid_total
+
+with open('input.txt', 'r') as f:
+    data = f.read().split()
+    temp_data = list(map(int, data))
+    # PART 1
+    print(count_meta(temp_data, 0))
+
+    # PART 2
+    temp_data = list(map(int, data))
+    lookup_dict = count_meta2(temp_data, 0, {}, 0)
+    print(lookup_dict)
+
