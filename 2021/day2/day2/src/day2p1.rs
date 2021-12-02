a432c1cf3f1ae81dd24412a40ae0d9a8eb02a47a
Chuck Woodraska <chuck.woodraska@gmail.com>
2021-12-02 18:31:44 -0500

    Day 2 2021 Rust

---
+++ 2021/day2/day2/src/day2p1.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+struct Instruction {
+    direction: String,
+    depth: i32
+}
+
+const FNAME: &str = "../../input.txt";
+fn main() {
+    let f = File::open(FNAME).expect("Unable to open data file");
+    let reader = BufReader::new(f);
+    let vec: Vec<String> = reader
+        .lines()
+        .collect::<Result<_, _>>()
+        .unwrap();
+
+
+    let instructions: Vec<Instruction> = vec.into_iter()
+        .map(|x| {
+            let (direction, depth) = x
+                .split_once(' ')
+                .unwrap();
+
+            Instruction {
+                direction: direction.to_string(),
+                depth: depth.parse().unwrap(),
+            }
+        })
+        .collect();
+
+    let mut h = 0;
+    let mut d = 0;
+    for i in instructions {
+        match i.direction.as_str() {
+            "down" => d += i.depth,
+            "up" => d -= i.depth,
+            "forward" => {
+                h += i.depth;
+            }
+            _ => println!("something else!"),
+        }
+    }
+    println!("{}", h*d);
+}
